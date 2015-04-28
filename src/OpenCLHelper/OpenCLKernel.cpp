cac60c89d07398869391683e779f06b8b34b693c
mihed <mihedb@gmail.com>
2015-04-28 11:47:35 +0200

    Add OpenCLKernel to encapsulate kernel calls

---
+++ src/OpenCLHelper/OpenCLKernel.cpp
@@ -0,0 +1,23 @@
+/*
+ * OpenCLKernel.cpp
+ *
+ *  Created on: Apr 28, 2015
+ *      Author: Mikael
+ */
+
+#include "OpenCLKernel.h"
+
+namespace ATML {
+namespace Helper {
+
+OpenCLKernel::OpenCLKernel() {
+	// TODO Auto-generated constructor stub
+
+}
+
+OpenCLKernel::~OpenCLKernel() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Helper */
+} /* namespace ATML */
---
+++ src/OpenCLHelper/OpenCLKernel.h
@@ -0,0 +1,23 @@
+/*
+ * OpenCLKernel.h
+ *
+ *  Created on: Apr 28, 2015
+ *      Author: Mikael
+ */
+
+#ifndef ATML_OPENCLHELPER_OPENCLKERNEL_H_
+#define ATML_OPENCLHELPER_OPENCLKERNEL_H_
+
+namespace ATML {
+namespace Helper {
+
+class OpenCLKernel {
+public:
+	OpenCLKernel();
+	virtual ~OpenCLKernel();
+};
+
+} /* namespace Helper */
+} /* namespace ATML */
+
+#endif /* ATML_OPENCLHELPER_OPENCLKERNEL_H_ */
