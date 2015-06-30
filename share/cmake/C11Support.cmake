8ec377e48068a9f2d217384a5a1bf3f40a90ee14
mihed <mihedb@gmail.com>
2015-06-30 11:48:20 +0200

    Add multithreading support to GNU compiler

--- share/cmake/C11Support.cmake
+++ share/cmake/C11Support.cmake
@@ -12,3 +12,7 @@ else()
 			message("The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
 		endif()
 endif()
+
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+	SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
+endif()
